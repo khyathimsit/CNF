995ca3fca5eb4cbaceaabb5b3730683ba04627b9
khyathimsit <khyathi17@msitprogram.net>
2018-12-18 19:32:31 +0530

    Assignment 1

--- Module-8/Assignment1/tcpServer.py
+++ Module-8/Assignment1/tcpServer.py
@@ -1,40 +1,28 @@
 import socket
-
-def convert(data):
-    tokens = data.split(" ")
-    if (tokens[1] == "INR") :
-        convert = int(tokens[2])/67
-    if (tokens[1] == "Yen") :
-        convert = int(tokens[2])/113.47
-    if (tokens[1] == "Pound") :
-        convert = int(tokens[2])/0.75
-    if (tokens[1] == "Dollar"):
-        if(tokens[0] == "INR"):
-            convert = int(tokens[2])*67
-        if(tokens[0] == "Yen"):
-            convert = int(tokens[2])*113.47
-        if(tokens[0] == "Pound"):
-            convert = int(tokens[2])*0.75
-    return convert
-    
 def main():
     host  = '127.0.0.1'
     port = 5000
+    dict = {"INRTODollar" : 0.0149,"DollarTOINR" : 67, "DollarTOPounds" : 0.75, 
+            "PoundsTODollar" : 1.3333, "DollarTOYen" : 113.41, "YenTODollar" : 0.0088}
     s = socket.socket()
     s.bind((host,port))
     s.listen(1)
     c, addr = s.accept()
     print ('connection from : '+ str(addr))
     while True :
-        data = c.recv(1024)
-        data = data.decode()
+        data = c.recv(1024).decode()
         if not data:
             break
         print ("from connected user : "+ str(data))
-        data = str(data)
-        currency = convert(data)
-        print('encoding:'+ str(currency))
-        c.send(str(currency).encode())
+        data = data.split(" ")
+        # if(list1[1] == 'INR' and list1[4] == 'Dollar'):
+        #   result = int(list1[2])/67
+        # elif(list1[1] == 'Dollar' and list1[4] == 'Pounds'):
+        #   result = int(list1[2])*0.75
+        result = int(data[2])*(dict[data[1]+data[3]+data[4]])
+        result = round(result, 1)
+        print('sending: '+ str(result))
+        c.send(str(result).encode())
     c.close()
 
 if __name__ == '__main__':
